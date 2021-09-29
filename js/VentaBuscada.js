47a9ae60e465bc20e071ada8de8d21f762c7e750
Cristian Gonzalo Sarmiento Castro <sarmientocastrocris@gmail.com>
2021-09-29 16:13:27 -0500

    Todas las busquedas funcionales admin Ventas

--- js/VentaBuscada.js
+++ js/VentaBuscada.js
@@ -1,11 +1,5 @@
-console.log('Ventana Buscada Presente')
-
 function InsertarDatosModalVentaBuscada(id) {
-    
     var id = id;
-    console.log(id)
-
-
     const BusquedaId = document.getElementById("IdBusqueda")
     const BusquedaArticulo = document.getElementById("ArticuloBusqueda")
     const BusquedaCliente = document.getElementById("ClienteBusqueda")
@@ -14,7 +8,6 @@ function InsertarDatosModalVentaBuscada(id) {
     const BusquedaFechaPago = document.getElementById("FechaPagoBusqueda")
     const BusquedaVendedor = document.getElementById("VendedorBusqueda")
     const BusquedaEstado = document.getElementById("EstadoBusqueda")
-
     BusquedaId.textContent = Id[id]
     BusquedaArticulo.placeholder = Articulo[id]
     BusquedaCliente.placeholder = Cliente[id]
@@ -23,29 +16,11 @@ function InsertarDatosModalVentaBuscada(id) {
     BusquedaFechaPago.placeholder = FechaPago[id]
     BusquedaVendedor.placeholder = Vendedor[id]
     var valor = Estado[id]
-    console.log(valor)
-    var opciones= BusquedaEstado.options
-    
-    for (var opt, j=0; opt = opciones[j]; j++){
-        console.log(opciones[j])
-        console.log(opt.value)
-        console.log(BusquedaEstado.selectedIndex)
-        if (opt.value == valor){
-            console.log(BusquedaEstado.selectedIndex)
-            BusquedaEstado.selectedIndex  = j;
-            console.log(BusquedaEstado.selectedIndex)
-            console.log(valor)
-            console.log(opt.value)
-            console.log(opt)
-            console.log(j)
-            console.log(j.value)
-            console.log(opciones[j])
-           
+    var opciones = BusquedaEstado.options
+    for (var opt, j = 0; opt = opciones[j]; j++) {
+        if (opt.value == valor) {
+            BusquedaEstado.selectedIndex = j;
             break;
         }
-        BusquedaEstado.selectedIndex  = valor;
     }
-
-
-
 }

dfed29d7e1e6a73266c018b55897bbf3178dedd8
William Tavera (wstavera) <ws.tavera@gmail.com>
2021-10-07 14:40:17 -0500

    iniciando Firebase

---
+++ js/firebaseLogin.js
@@ -0,0 +1,84 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuuC5xt_cyNbakN_gIAJ3ixHvkw8LUCk8",
+  authDomain: "nightmare-mercurio.firebaseapp.com",
+  projectId: "nightmare-mercurio",
+  storageBucket: "nightmare-mercurio.appspot.com",
+  messagingSenderId: "829384661085",
+  appId: "1:829384661085:web:bddd58254813be754315b4",
+  measurementId: "G-TQYRLQ0VWT"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Declarar Variables globales
+const auth = firebase.auth()
+const proveedor = new firebase.auth.GoogleAuthProvider()
+const dataBase = firebase.firestore()
+let usuarioActual;
+let usuarioFoto;
+let usuarioEmail;
+
+
+//variables DOM
+const btnLogin = document.getElementById('boton-loginGmail')
+
+
+//funciones
+//login
+async function login(){
+    try{
+        const respuesta = await auth.signInWithPopup(proveedor)
+        /* console.log(respuesta) */
+        
+        usuarioActual = respuesta.user.displayName
+        usuarioFoto  = respuesta.user.photoURL
+        usuarioEmail = respuesta.user.email
+
+        console.log(usuarioActual, usuarioFoto, usuarioEmail)
+        
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function guardarUsuario(usuario){
+    try{
+        const respuesta = await dataBase.collection('ng_users').add(usuario)
+        return respuesta
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+//adicionar usurio
+async function adicionarUsuario(){
+    try{
+        const usuario = {
+            id: uuid.v4(), 
+            nombres: usuarioActual,
+            /* apellidos:, */
+            rol: '',
+            estado: false,
+            email:usuarioEmail,
+            photoUrl:usuarioFoto, 
+
+        }
+        const respuesta = await guardarUsuario(usuario)
+        console.log(respuesta)
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+
+//evento
+btnLogin.addEventListener('click', (e)=>{
+    e.preventDefault()
+    login()
+    /* adicionarUsuario() */
+})
+

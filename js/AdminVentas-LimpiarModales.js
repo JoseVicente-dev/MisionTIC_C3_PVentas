ea33c435b48c164b3f25f3f3d7f6b2bd5a8855ae
FervKa <90405746+FervKa@users.noreply.github.com>
2021-10-07 16:37:25 -0500

    Holi

---
+++ js/AdminVentas-LimpiarModales.js
@@ -0,0 +1,33 @@
+function limpiarBusqueda(){
+    // modal nueva venta
+    document.getElementById("IdNuevo").value =""
+    document.getElementById("ArticuloNuevo").value=""
+    document.getElementById("ClienteNuevo").value=""
+    document.getElementById("ValorNuevo").value=""
+    document.getElementById("FechaVentaNuevo").value=""
+    document.getElementById("FechaPagoNuevo").value =""
+    document.getElementById("VendedorNuevo").value=""
+    // document.getElementById("EstadoNuevo").value= 1
+    // modal modificar venta
+    // document.getElementById("IdBusqueda").value =""
+    // document.getElementById("ArticuloBusqueda").value=""
+    // document.getElementById("ClienteBusqueda").value=""
+    // document.getElementById("ValorBusqueda").value =""
+    // document.getElementById("FechaVentaBusqueda").value=""
+    // document.getElementById("FechaPagoBusqueda").value=""
+    // document.getElementById("VendedorBusqueda").value =""
+    // document.getElementById("EstadoBusqueda").value= 1
+    // modal resultado busqueda
+    document.getElementById("IdResultado").value =""
+    document.getElementById("ArticuloResultado").value=""
+    document.getElementById("ClienteResultado").value=""
+    document.getElementById("ValorResultado").value =""
+    document.getElementById("FechaVentaResultado").value=""
+    document.getElementById("FechaPagoResultado").value=""
+    document.getElementById("VendedorResultado").value =""
+    // document.getElementById("EstadoResultado").value= 1
+    // modal buscar ventas
+    document.getElementById("IdVentaBuscada").value =""
+    document.getElementById("NombreVentaBuscada").value="" 
+    
+}
---
+++ js/AdminVentas-VentaBuscada.js
@@ -0,0 +1,26 @@
+function InsertarDatosModalVentaBuscada(id) {
+    var id = id;
+    const BusquedaId = document.getElementById("IdBusqueda")
+    const BusquedaArticulo = document.getElementById("ArticuloBusqueda")
+    const BusquedaCliente = document.getElementById("ClienteBusqueda")
+    const BusquedaValor = document.getElementById("ValorBusqueda")
+    const BusquedaFechaVenta = document.getElementById("FechaVentaBusqueda")
+    const BusquedaFechaPago = document.getElementById("FechaPagoBusqueda")
+    const BusquedaVendedor = document.getElementById("VendedorBusqueda")
+    const BusquedaEstado = document.getElementById("EstadoBusqueda")
+    BusquedaId.textContent = Id[id]
+    BusquedaArticulo.placeholder = Articulo[id]
+    BusquedaCliente.placeholder = Cliente[id]
+    BusquedaValor.placeholder = Valor[id]
+    BusquedaFechaVenta.placeholder = FechaVenta[id]
+    BusquedaFechaPago.placeholder = FechaPago[id]
+    BusquedaVendedor.placeholder = Vendedor[id]
+    var valor = Estado[id]
+    var opciones = BusquedaEstado.options
+    for (var opt, j = 0; opt = opciones[j]; j++) {
+        if (opt.value == valor) {
+            BusquedaEstado.selectedIndex = j;
+            break;
+        }
+    }
+}

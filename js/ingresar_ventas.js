3644f19fd4564b1156e6c5e7ba434fa9009b207d
FervKa <90405746+FervKa@users.noreply.github.com>
2021-10-08 16:37:08 -0500

    Se agrega documento js

---
+++ js/ingresar_ventas.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuuC5xt_cyNbakN_gIAJ3ixHvkw8LUCk8",
+    authDomain: "nightmare-mercurio.firebaseapp.com",
+    projectId: "nightmare-mercurio",
+    storageBucket: "nightmare-mercurio.appspot.com",
+    messagingSenderId: "829384661085",
+    appId: "1:829384661085:web:bddd58254813be754315b4",
+    measurementId: "G-TQYRLQ0VWT"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+//Variables usadas:
+const dataBase = firebase.firestore()
+let id;
+let articulo;
+let cliente;
+let valor;
+let fecha_venta;
+let fecha_pago;
+let vendedor;
+let estado=false;
+

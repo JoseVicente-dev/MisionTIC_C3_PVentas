37bbed11158f7bfe5252b69ede333d45903e8131
Vicente Velasco <velasco.josevicente@gmail.com>
2021-10-08 16:39:19 -0500

    Se inicia conexion con Firebase para mostrar informacion del a BD

---
+++ js/Productos-MostrarInformacion.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuuC5xt_cyNbakN_gIAJ3ixHvkw8LUCk8",
+    authDomain: "nightmare-mercurio.firebaseapp.com",
+    projectId: "nightmare-mercurio",
+    storageBucket: "nightmare-mercurio.appspot.com",
+    messagingSenderId: "829384661085",
+    appId: "1:829384661085:web:bddd58254813be754315b4",
+    measurementId: "G-TQYRLQ0VWT"
+  };
+
+
+  

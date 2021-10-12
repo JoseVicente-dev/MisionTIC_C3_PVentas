cda8797a420fea1307f25f230da4e0f6ff2f3674
Cristian Gonzalo Sarmiento Castro <sarmientocastrocris@gmail.com>
2021-10-12 11:08:02 -0500

    restauracion funcionalidad de traer datos impelmentada

--- js/AdminVentas.js
+++ js/AdminVentas.js
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 const dataBase = firebase.firestore();
 // Declara Variables de DOM
 
-const btnNuevaventa= document.getElementById('btn_AgregarVenta');
+const btnNuevaventa= document.getElementById('btn_AgregarVenta')
 
 const btnBuscarVenta = document.getElementById('buscarVenta')
 const toastIngresoVenta = document.getElementById('liveToastIProduct')
@@ -107,42 +107,22 @@ async function mostrarInformacion() {
 
 // ------------------------------------------ Adicionar Ventas--------------------------------
 function AdicionrVenta(){
-    console.log('Inicio adiconar venta');
-    // Creacion de las variables de DOM
- /*    const idVentas= document.getElementById('IdNuevo'); */
+
+    const idVentas= document.getElementById('IdNuevo');
     const articuloVentas= document.getElementById('ArticuloNuevo');
     const clienteVentas= document.getElementById('ClienteNuevo');
     const ValorVentas= document.getElementById('ValorNuevo');
     const fechasVenta = document.getElementById('FechaVentaNuevo');
     const FechaPagoVentas = document.getElementById('FechaPagoNuevo');
-    const vendedor = document.getElementById('VendedorNuevo');
-    const estadoPago = document.getElementById('EstadoNuevo');
-
-    const ventaAgregar = {
-        id: uuid.v4(), 
-        nombres: articuloVentas,
-        cliente: clienteVentas,
-        valor: ValorVentas,
-        fechaVenta: fechasVenta,
-        fechaPago: FechaPagoVentas,
-        vendedor: vendedor,
-        estadoPago:estadoPago
-    } 
-
-    // Obtención de la base de datos
-    const VentasArray = [];
-    const DatosVentas = await dataBase.collection('ng_ventas').get()
-    Datosventas.forEach((t) => {
-        VentasArray.push(t.data());
-        console.log(t);
-    })
-
 
 }
+
+
+
 /* function showToast(id) {
     $(id).toast('show');
 } */
-/* async function obtenerDatos() {
+async function obtenerDatos() {
     try {
         const inputDescription = document.getElementById("inputDescripcion").value.replace(/^\w/, (c) => c.toUpperCase());
         const inputWeigth = document.getElementById("inputPeso").value;
@@ -191,7 +171,7 @@ async function anadirVenta(product) {
         console.log(error);
     }
 }
-mostrarInformacion() */
+mostrarInformacion()
 /* ---------------------------------------------------------------------------------------------- */
 //Actiualizar Ventas en la tabla
 /* async function actualizar() {
